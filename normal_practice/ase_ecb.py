fcb616b1a6c68413f02de39d48180ba8bd7f319e
bill0812 <maxwell111023@gmail.com>
2018-09-20 18:14:38 +0800

    first commit to update some practice

---
+++ normal_practice/ase_ecb.py
@@ -0,0 +1,21 @@
+# aaaaaaaaaaaaaaaa aaaa 8 bbbb bbbbbbbbbbbbbZE4
+
+# 19 20
+
+# a5a0127d411e272e53688a857f17c094
+# 394a76f53aa489253d1927269d677ee7
+# 2fbe45c053d47f2f273d6b7829a8447c
+# 0565fd240f33ca94f5e15b3bed80343e
+ 
+# aaaaaaaaaaaaaaaa aa 8 bbbbbb bbbbbbbbbbbb ????
+
+# 18 18
+
+# a5a0127d411e272e53688a857f17c094
+# a5d03f9ae880c0b8c82be1c4fef3b712
+# 2fbe45c053d47f2f273d6b7829a8447c
+# 6caaacf999210e1b2eda2cbd57587a5a
+
+# aaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbb
+
+# aaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbZE4
